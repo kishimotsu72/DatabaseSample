4797026689a9fe3163cdfdce244825af1293ab02
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-21 10:32:03 +0900

    P242 4>アクティビティにデータベース処理以外のコードを記述する acticity_cocktail_memo.xml 45行目 id= 修正

--- app/src/main/java/com/websarva/wings/android/databasesample/CooktailMemoActivity.java
+++ app/src/main/java/com/websarva/wings/android/databasesample/CooktailMemoActivity.java
@@ -1,13 +1,68 @@
 package com.websarva.wings.android.databasesample;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class CooktailMemoActivity extends AppCompatActivity {
+
+    int _cocktailId = -1;
+
+    String _cocktailName = "";
+
+    TextView _tvCocktailName;
+
+    Button _btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooktail_memo);
+
+        _tvCocktailName = findViewById(R.id.tvCocktailName);
+
+        _btnSave = findViewById(R.id.btnSave);
+
+        ListView lvCocktail = findViewById(R.id.lvCocktail);
+
+        lvCocktail.setOnItemClickListener(new ListItemClickListener());
+
+    }
+
+    public void onSaveButtonClick(View view){
+
+        EditText etNote = findViewById(R.id.etNote);
+
+        _tvCocktailName.setText(getString(R.string.tv_name));
+
+        etNote.setText("");
+
+        _btnSave.setEnabled(false);
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?>parent,View view,int position,long id){
+
+            _cocktailId = position;
+
+            _cocktailName = (String)parent.getItemAtPosition(position);
+
+           _tvCocktailName.setText(_cocktailName);
+
+           _btnSave.setEnabled(true);
+
+        }
     }
 }
