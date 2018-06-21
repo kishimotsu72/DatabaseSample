3d37a420bdf51ea5943c1741a577bf93e0c93d26
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-21 09:44:35 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/databasesample/CooktailMemoActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.databasesample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CooktailMemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cooktail_memo);
+    }
+}
